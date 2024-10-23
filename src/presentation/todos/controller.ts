1c340933523136b4d8bc9274c6774f6bb263f34c
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-22 21:13:17 -0600

    Implement a rout for get "todos" using MVC

---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+export class TodosController {
+  constructor() {}
+
+  public getTodos = (req: Request, res: Response) => {
+    res.json([
+      { id: 1, text: 'Buy milk', createdAt: new Date() },
+      { id: 1, text: 'Buy Bread', createdAt: null },
+      { id: 1, text: 'Buy butter', createdAt: new Date() },
+    ]);
+  };
+}
