07a0cc99b8b429c6d98b1f048a01c6e90ba25a2e
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-24 02:18:30 -0600

    Create new static function in todo entity to create a new instance from an object

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -4,4 +4,19 @@ export class TodoEntity {
   get isCompleted() {
     return !!this.completedAt;
   }
+
+  public static fromObject(object: { [key: string]: any }): TodoEntity {
+    const { id, text, completedAt } = object;
+
+    if (!id || typeof id !== 'number') throw 'id is required and must be a number';
+    if (!text) throw 'Text is required';
+
+    let newCompletedAt;
+    if (completedAt) {
+      newCompletedAt = new Date(completedAt);
+      if (isNaN(newCompletedAt.getTime())) throw 'completedAt is not a valid date';
+    }
+
+    return new TodoEntity(id, text, newCompletedAt);
+  }
 }
